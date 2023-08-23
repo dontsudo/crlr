df6ab23d571ceb614a16fe034728f06341fb959a
superposition82 <chotnt741@gmail.com>
2023-08-23 20:25:44 +0900

    retry jobs when failed

--- index.js
+++ index.js
@@ -7,22 +7,54 @@ const {
   objectToMultipleSheetExcel,
 } = require("./lib/helpers.js");
 
-async function run() {
-  retrieveCarsInformation().then((result) => {
-    createExcelSheet("danawa.xlsx", "다나와 신차정보", result);
-  });
+const MAX_RETRIES = 3; // Maximum number of retries for each job
 
-  retrievePostsAndComments().then((result) => {
-    createExcelSheet("doctor-cha.xlsx", "닥터차 포스트", result);
-  });
+async function retryJob(jobFunction, jobName) {
+  let retries = 0;
+  while (retries < MAX_RETRIES) {
+    try {
+      const result = await jobFunction();
+      console.log(`${jobName} succeeded on attempt ${retries + 1}`);
+      return result;
+    } catch (error) {
+      console.error(`${jobName} failed on attempt ${retries + 1}: ${error}`);
+      retries++;
+    }
+  }
 
-  retrieveCarTerms().then((result) => {
-    createExcelSheet("terms-naver.xlsx", "네이버 사전용어", result);
-  });
-
-  retrieveGongimTables().then((result) => {
-    objectToMultipleSheetExcel("dsmotors.xlsx", result);
-  });
+  console.error(`${jobName} failed after ${MAX_RETRIES} attempts`);
+  return null; // You can handle this case as needed
 }
 
-run();
+async function runJobs() {
+  const danawaResult = await retryJob(
+    retrieveCarsInformation,
+    "retrieveCarsInformation"
+  );
+  if (danawaResult) {
+    createExcelSheet("danawa.xlsx", "다나와 신차정보", danawaResult);
+  }
+
+  const doctorChaResult = await retryJob(
+    retrievePostsAndComments,
+    "retrievePostsAndComments"
+  );
+  if (doctorChaResult) {
+    createExcelSheet("doctor-cha.xlsx", "닥터차 포스트", doctorChaResult);
+  }
+
+  const termsNaverResult = await retryJob(retrieveCarTerms, "retrieveCarTerms");
+  if (termsNaverResult) {
+    createExcelSheet("terms-naver.xlsx", "네이버 사전용어", termsNaverResult);
+  }
+
+  const dsmotorsResult = await retryJob(
+    retrieveGongimTables,
+    "retrieveGongimTables"
+  );
+  if (dsmotorsResult) {
+    objectToMultipleSheetExcel("dsmotors.xlsx", dsmotorsResult);
+  }
+}
+
+runJobs();
